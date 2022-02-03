77fdc2a2fd9a848298a8dc88354b237f3b71bc74
Anson <git@anson-cheung.com>
2022-02-03 12:46:26 +0800

    Add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const BearIsland = await ethers.getContractFactory("BearIsland");
+  const baseIsland = await BearIsland.deploy("https://replace-me.com/");
 
-  await greeter.deployed();
+  await baseIsland.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("BearIsland deployed to:", baseIsland.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
