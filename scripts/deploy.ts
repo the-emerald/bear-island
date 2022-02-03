bea70785cac26eadc0cff8f25a9aade36baf2209
Anson <git@anson-cheung.com>
2022-02-03 12:51:44 +0800

    Add verification to deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,13 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { run, ethers } from "hardhat";
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const BASE_URI = "https://replace-me.com/";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -15,11 +21,21 @@ async function main() {
 
   // We get the contract to deploy
   const BearIsland = await ethers.getContractFactory("BearIsland");
-  const baseIsland = await BearIsland.deploy("https://replace-me.com/");
+  const bearIsland = await BearIsland.deploy(BASE_URI);
 
-  await baseIsland.deployed();
+  await bearIsland.deployed();
 
-  console.log("BearIsland deployed to:", baseIsland.address);
+  console.log("BearIsland deployed to:", bearIsland.address);
+  console.log("Waiting 75 seconds... do not exit the script.");
+
+  // 75s = 5 blocks
+  await delay(75000);
+
+  await run("verify:verify", {
+    address: bearIsland.address,
+    constructorArguments: [BASE_URI]
+  });
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
